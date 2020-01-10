f44bfdf83644538f97d852393edcce6175431464
Jacob Baca <52418765+jacobrobertbaca@users.noreply.github.com>
2020-01-10 13:54:19 -0500

    changed setup.py, changed all instance of 'since' to 'start_date' (#1)
    * changed setup.py, changed all instance of 'since' to 'start_date'
    
    * changed version and added extras_require
    
    * changed license to AGPL

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
 import setuptools
 
 
-def get_version():
-    version = {}
-    with open('tap_pagerduty/version.py') as fp:
-        exec(fp.read(), version)
-    return version['__version__']
-
-
-with open('README.md', 'r') as f:
-    readme = f.read()
-
-
 setuptools.setup(
     name='tap_pagerduty',
     author='David Wallace',
     author_email='david.wallace@goodeggs.com',
-    version=get_version(),
+    version='0.1.0',
     url='https://github.com/goodeggs/tap-pagerduty',
     description='Singer.io tap for extracting data from Pagerduty v2 API',
-    long_description=readme,
+    long_description='',
     long_description_content_type='text/markdown',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
@@ -41,6 +30,12 @@ setuptools.setup(
         'requests==2.22.0',
         'singer-python==5.7.0'
     ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'ipdb'
+        ]
+    },
     python_requires='>=3.6',
     entry_points={
         'console_scripts': ['tap-pagerduty = tap_pagerduty:main']
